69c1995d7e114ff207f90b5771a8267a17ab3db9
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-02-27 15:01:25 +0300

    Перевод theme

--- src/components/ThemeReference.js
+++ src/components/ThemeReference.js
@@ -1,19 +1,19 @@
 import { defaultConfig } from '@/utils/defaultConfig'
 
 const descriptions = {
-  screens: "Your project's responsive breakpoints",
-  colors: "Your project's color palette",
-  spacing: "Your project's spacing scale",
-  container: 'Configuration for the `container` plugin',
-  inset: 'Values for the `top`, `right`, `bottom`, and `left` properties',
-  placeholderColor: 'Values for the `placeholderColor` plugin',
-  placeholderOpacity: 'Values for the `placeholderOpacity` plugin',
-  rotate: 'Values for the `rotate` plugin',
-  scale: 'Values for the `scale` plugin',
-  skew: 'Values for the `skew` plugin',
-  space: 'Values for the `space` plugin',
-  textOpacity: 'Values for the `textOpacity` plugin',
-  translate: 'Values for the `translate` plugin',
+  screens: "Адаптивные контрольные точки Вашего проекта",
+  colors: "Цветовая палитра вашего проекта",
+  spacing: "Шкала интервалов вашего проекта",
+  container: 'Конфигурация плагина `container`',
+  inset: 'Значения свойств `top`, `right`, `bottom` и `left`',
+  placeholderColor: 'Значения для плагина `placeholderColor`',
+  placeholderOpacity: 'Значения для плагина `placeholderOpacity`',
+  rotate: 'Значения для плагина `rotate`',
+  scale: 'Значения для плагина `scale`',
+  skew: 'Значения для плагина `skew`',
+  space: 'Значения для плагина `space`',
+  textOpacity: 'Значения для плагина `textOpacity`',
+  translate: 'Значения для плагина `translate`',
 }
 
 export function ThemeReference() {
@@ -39,11 +39,10 @@ export function ThemeReference() {
                     i % 2 === 0 ? segment : <code key={i}>{segment}</code>
                   ) || (
                   <>
-                    Values for the{' '}
+                    Значения для свойств{' '}
                     <code>
                       {key.replace(/([a-z])([A-Z])/g, (_m, p1, p2) => `${p1}-${p2.toLowerCase()}`)}
-                    </code>{' '}
-                    property
+                    </code>
                   </>
                 )}
               </td>
