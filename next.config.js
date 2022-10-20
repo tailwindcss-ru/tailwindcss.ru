1f85b686ab85f30e91d38ccf9902ccfe852585fd
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-10-20 17:07:57 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ const fallbackGetStaticProps = {
 }
 
 module.exports = withBundleAnalyzer({
-  exportTrailingSlash: true,
+  trailingSlash: true,
   swcMinify: true,
   pageExtensions: ['js', 'jsx', 'mdx'],
   experimental: {
