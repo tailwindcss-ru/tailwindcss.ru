be1fc34ad98ac6e47858ad3f9e1f626d4b5501a4
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-09-07 16:09:17 +0300

    GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const FAVICON_VERSION = 3
 
@@ -46,11 +47,7 @@ export default class Document extends NextDocument {
             }}
           />
           {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MB9DQ39');`,
-            }}
-          ></script>
+          <GoogleTagManager gtmId="GTM-MB9DQ39" />
           {/* End Google Tag Manager */}
         </Head>
         <body
